910391c464705a8f31826ac95f052c6f5a1d2b82
AnilHash <anilaraganji123@gmail.com>
2024-04-17 11:35:36 +0530

    feat: displaying event object

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 module.exports.handler = async (event) => {
-  console.log("[INFO] Event Object --> ", event);
+  console.log(
+    "[INFO] Event Object --> ",
+    JSON.stringify(event),
+    "OPENED EVENT",
+    event["Records"][0]["s3"]
+  );
 };
